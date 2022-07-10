5c42e725a400e8db5d420163435094f7f62b732d
Tushar Aggarwal <beingatushar@gmail.com>
2022-07-10 23:35:49 +0530

    Update script.js

--- Day-2/script.js
+++ Day-2/script.js
@@ -3,7 +3,7 @@ const prev = document.querySelector("#prev");
 const bars = document.querySelectorAll(".bar");
 const steps = document.querySelectorAll(".step");
 let step = 0;
-let maxSteps = 3;
+let maxSteps = steps.length - 1;
 
 function check() {
     if (step === 0) {
@@ -13,21 +13,23 @@ function check() {
     } else {
         next.disabled = false;
         prev.disabled = false;
+
     }
 }
 check();
-next.addEventListener('click', () => {
-    step++;
-    check();
-    console.log(step);
-    bars[step - 1].classList.toggle("bar-active");
-    steps[step].classList.toggle("step-active");
-})
-prev.addEventListener('click', () => {
-    step--;
-    check();
-    console.log(step);
-    bars[step].classList.toggle("bar-active");
-    steps[step + 1].classList.toggle("step-active");
 
-})
+
+function toggleClass(elements, name, count) {
+    elements[count].classList.toggle(`${name}-active`);
+}
+
+function clickButton(action) {
+    let isNext = (action === "next");
+    if (isNext) step++;
+    else step--;
+    check();
+    toggleClass(bars, "bar", (isNext ? step - 1 : step));
+    toggleClass(steps, "step", (isNext ? step : step + 1));
+}
+next.addEventListener("click", () => clickButton("next"));
+prev.addEventListener("click", () => clickButton("prev"));
