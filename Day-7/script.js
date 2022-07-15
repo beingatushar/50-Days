1594d5d2f3f42ffae7b779b34f5dc504c2a0d77a
Tushar Aggarwal <beingatushar@gmail.com>
2022-07-15 23:39:40 +0530

    Update script.js

--- Day-7/script.js
+++ Day-7/script.js
@@ -1,12 +1,12 @@
 const ps = document.querySelector(".ps");
 const xbox = document.querySelector(".xbox");
-ps.addEventListener("click", () => {
+ps.addEventListener("mouseenter", () => {
     ps.classList.remove("inactive");
     ps.classList.add("active");
     xbox.classList.add("inactive");
     xbox.classList.remove("active");
 })
-xbox.addEventListener("click", () => {
+xbox.addEventListener("mouseenter", () => {
     xbox.classList.remove("inactive");
     xbox.classList.add("active");
     ps.classList.add("inactive");
