74cc4b0358e2a7704d590838bc25e10a06708a87
Tushar Aggarwal <beingatushar@gmail.com>
2022-08-01 21:41:07 +0530

    Create boilerplate.js
    Used for creating template code

---
+++ boilerplate.js
@@ -0,0 +1,34 @@
+const htmlContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <script src="script.js"></script>
+</body>
+
+</html>`
+const cssContent = `* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    min-height: 100vh;
+}`;
+const process = require('process');
+const fs = require("fs");
+const folderNames = process.argv.slice(2);
+const path = process.mainModule.path;
+folderNames.forEach(folder => {
+    fs.mkdirSync(`${path}//${folder}`)
+    fs.writeFileSync(`${folder}//index.html`, htmlContent, error => console.log(error))
+    fs.writeFileSync(`${folder}//style.css`, cssContent, error => console.log(error))
+    fs.writeFileSync(`${folder}//script.js`, '', error => console.log(error))
+});
